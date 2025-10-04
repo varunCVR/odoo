a4b2d7382b85604470ff212576e8d7a0eb2c4bd2
varunCVR <varunp.cienciasvr@gmail.com>
2025-10-04 15:44:18 +0530

    [Branch Issue]

--- expense-backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [PrismaModule, AuthModule],
-})
-export class AppModule {}
